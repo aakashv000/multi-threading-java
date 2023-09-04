3d45b3080e2d242cdc5c5ceabe87d1820a58b0df
Verma, Aakash <aakash.verma@intelligrated.com>
2023-09-04 08:26:09 +0530

    basic thread

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
+
+        Thread t = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("In thread: " + Thread.currentThread());
+                        System.out.println("Current thread priority = " + Thread.currentThread().getPriority());
+                    }
+                }
+        );
+
+        t.setName("My thread first");
+        t.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println("Before starting custom thread. Thread = " + Thread.currentThread());
+        t.start();
+        System.out.println("After starting custom thread. Thread = " + Thread.currentThread());
+
+        Thread.sleep(5*1000);
     }
 }
