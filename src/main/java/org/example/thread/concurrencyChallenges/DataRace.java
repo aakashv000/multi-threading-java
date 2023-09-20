3be17dda0b94248d62b8489b2d8f967b12585dc9
Verma, Aakash <aakash.verma@intelligrated.com>
2023-09-20 13:10:26 +0530

    data race - observe problem

--- src/main/java/org/example/thread/concurrencyChallenges/DataRace.java
+++ src/main/java/org/example/thread/concurrencyChallenges/DataRace.java
@@ -26,10 +26,10 @@ public class DataRace {
     private static class SharedClass {
         // making a variable volatile, is equivalent to memory fence/barrier
         // and thus, is a solution for our data race here
-//        private int x;
-//        private int y;
-        private volatile int x;
-        private volatile int y;
+        private int x;
+        private int y;
+//        private volatile int x;
+//        private volatile int y;
 
         // 2nd solution - make increment() method synchronized
         private void increment() {
