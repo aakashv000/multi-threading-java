8f2273692251cb65ceceaeaaf0e84a133374ae06
Verma, Aakash <aakash.verma@intelligrated.com>
2023-10-27 16:27:40 +0530

    reentrantReadWriteLock - performance - concurrent read-only's

--- src/main/java/org/example/thread/lock/ReentrantReadWriteLock_perf.java
+++ src/main/java/org/example/thread/lock/ReentrantReadWriteLock_perf.java
@@ -5,11 +5,19 @@ import java.util.List;
 import java.util.NavigableMap;
 import java.util.Random;
 import java.util.TreeMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class ReentrantReadWriteLock {
+/**
+ * Compare performance of read-heavy multi-threaded systems,
+ * using Reentrant Read & Write Lock separately for concurrent reads
+ * vs ReentrantLock
+ */
+public class ReentrantReadWriteLock_perf {
     public static void main(String[] args) throws InterruptedException {
         final int MAX_PRICE = 1000;
 
@@ -39,7 +47,7 @@ public class ReentrantReadWriteLock {
         final int N_READER_THREADS = 7;
 
         List<Thread> readers = new ArrayList<>();
-        for (int i : IntStream.range(0, 7).boxed().collect(Collectors.toList())) {
+        for (int i : IntStream.range(0, N_READER_THREADS).boxed().collect(Collectors.toList())) {
             Thread reader = new Thread(() -> {
                 for (int j : IntStream.range(0, 100000).toArray()) {
                     int upperBound = random.nextInt(MAX_PRICE);
@@ -64,6 +72,9 @@ public class ReentrantReadWriteLock {
 
         long endReadTime = System.currentTimeMillis();
         System.out.println("Total reading time = " + (endReadTime - startReadTime));
+        // Result:
+        // reentrantLock ~= 13k ms
+        // reentrantReadWriteLock ~= 3k ms
     }
 }
 
@@ -73,10 +84,16 @@ public class ReentrantReadWriteLock {
 class InventoryDatabase {
     private TreeMap<Integer, Integer> priceCountMap = new TreeMap<>();
 
-    private ReentrantLock reentrantLock = new ReentrantLock();
+    // toggle
+//    private ReentrantLock reentrantLock = new ReentrantLock();
+    private ReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+    private Lock readLock = reentrantReadWriteLock.readLock();
+    private Lock writeLock = reentrantReadWriteLock.writeLock();
 
     public int getNumberOfItemsInPriceRange (int lowerBound, int upperBound) {
-        reentrantLock.lock();
+        // toggle
+//        reentrantLock.lock();
+        readLock.lock();
         try {
             Integer fromKey = priceCountMap.ceilingKey(lowerBound);
             Integer toKey = priceCountMap.floorKey(upperBound);
@@ -88,23 +105,31 @@ class InventoryDatabase {
             return sum;
         }
         finally {
-            reentrantLock.unlock();
+            // toggle
+//            reentrantLock.unlock();
+            readLock.unlock();
         }
     }
 
     public void addItem (int price) {
-        reentrantLock.lock();
+        // toggle
+//        reentrantLock.lock();
+        writeLock.lock();
         try {
             Integer nItemsForPrice = priceCountMap.getOrDefault(price, 0);
             priceCountMap.put(price, nItemsForPrice + 1);
         }
         finally {
-            reentrantLock.unlock();
+            // toggle
+//            reentrantLock.unlock();
+            writeLock.unlock();
         }
     }
 
     public void removeItem (int price) {
-        reentrantLock.lock();
+        // toggle
+//        reentrantLock.lock();
+        writeLock.lock();
         try {
             Integer nItemsForPrice = priceCountMap.get(price);
             if (nItemsForPrice == null || nItemsForPrice == 1)
@@ -113,7 +138,9 @@ class InventoryDatabase {
                 priceCountMap.put(price, nItemsForPrice - 1);
         }
         finally {
-            reentrantLock.unlock();
+            // toggle
+//            reentrantLock.unlock();
+            writeLock.unlock();
         }
     }
 }
